74c2c4183a1026274eb35edff15134b0dd153c2b
Suganya28J <suganya030522@gmail.com>
2026-08-12 18:04:35 +0530

    updated

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,7 +5,7 @@ import NextAuthProvider from "@/providers/NextAuthProvider";
 import ThemeProvider from "@/providers/ThemeProvider";
 
 const poppins = Poppins({ 
-  weight: ['300', '400', '500', '600', '700', '800'], 
+  weight: ['300', '400', '500', '600', '700', '800', '900'], 
   subsets: ["latin"], 
   display: "swap",
   variable: "--font-poppins"
